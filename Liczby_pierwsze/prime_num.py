fd32c647635e6ba576c2e7cf8210712695fdb99d
Kacper Strugała <kacperstrugala5@gmail.com>
2022-12-13 20:58:50 +0100

    Matura zadania update

--- Liczby_pierwsze/prime_num.py
+++ Liczby_pierwsze/prime_num.py
@@ -1,9 +1,4 @@
 
-
-
-
-
-import math
 def prime(a):
     if a < 2:
         return False
@@ -17,34 +12,3 @@ def prime(a):
 a = int(input("Podaj liczbe: "))
 
 print(prime(a))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-import math
-def czyPierwsza(a):
-    if a < 2:
-        return False
-    for i in (range(2, int(a))):
-        if a % i == 0:
-            return False
-    return True
-
-
-print("Program sprawdzający, czy liczba jest liczbą pierwszą.")
-print("Podaj liczbę: ")
-a = int(input())
-print(czyPierwsza(a))
-'''
